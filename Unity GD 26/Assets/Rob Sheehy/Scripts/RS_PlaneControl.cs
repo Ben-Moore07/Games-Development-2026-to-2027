c3956ff7b58bc8a80be52a9c183ad540cbb09277
Robert Sheehy <ms0036625@mtu.ie>
2026-09-16 16:46:36 +0100

    Continued to work on plane movement and starting prefab
    Signed-off-by: Robert Sheehy <ms0036625@mtu.ie>

---
+++ Unity GD 26/Assets/Rob Sheehy/Scripts/RS_PlaneControl.cs
@@ -0,0 +1,67 @@
+using UnityEngine;
+
+public class RS_PlaneControl : MonoBehaviour
+{
+    float pitchingSpeed = 45f;  // Speed in degrees per second for pitching
+    private float rollingSpeed = 45f;
+    Vector3 velocity, acceleration;
+    private float thrustValue = 20f;
+    private float gravity = 9.81f;
+    float drag = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        acceleration = Vector3.zero;
+
+        float simulatedGravity;
+
+        if (velocity.magnitude < 10)
+            simulatedGravity = 9.8f;
+        else if (velocity.magnitude < 20)
+            simulatedGravity = 4f;
+        else simulatedGravity = 0;
+
+
+        acceleration += new Vector3(0, -simulatedGravity, 0);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            // Pitch
+            transform.Rotate(Vector3.right, pitchingSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            // Pitch
+            transform.Rotate(Vector3.right, -pitchingSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            // Pitch
+            transform.Rotate(Vector3.forward, rollingSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // Pitch
+            transform.Rotate(Vector3.forward, -rollingSpeed * Time.deltaTime);
+        }
+
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            acceleration += transform.forward * thrustValue;
+        }
+
+        acceleration += -drag* velocity;
+
+        velocity += acceleration * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;
+
+
+    }
+}
