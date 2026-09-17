dfe0c82ec117c619c58877ef9cd47fda082f1f09
Student Richard Doyle (TL_KCOMP_B_Y2) <t00285062@mymtu.ie>
2026-09-17 12:57:25 +0100

    17/09/26
    Signed-off-by: Student Richard Doyle (TL_KCOMP_B_Y2) <t00285062@mymtu.ie>

---
+++ Unity GD 26/Assets/RIchard Doyle/Scripts/RD_followCameraScript.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class RD_followCameraScript : MonoBehaviour
+{
+    //Method1
+    //public Transform thePlane;          //Public variable appears on script in unity editor, have to drag appropriate item from hierarchy to the slot in the inspector.
+                                        //Not good for multiple objects or instantiated 
+    //Method 2
+    RD_movementScript thePlaneScript;
+    Transform thePlane;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        thePlaneScript = FindAnyObjectByType<RD_movementScript>();
+        thePlane = thePlaneScript.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            thePlaneScript.TurnRed();
+        }
+
+        transform.position = Vector3.Lerp(transform.position, thePlane.transform.position - 10 * thePlane.forward + 2 * thePlane.up, 0.015f);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, thePlane.rotation, 0.015f);
+
+    }
+}
